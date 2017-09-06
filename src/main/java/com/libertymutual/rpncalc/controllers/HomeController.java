7d391cef3b0db4314d654e40d011c44f7ca6cf27
Roman Galanti <roman.galanti@libertymutual.com>
2017-09-06 10:09:27 -0700

    Added HomePageController to redirect to /calculator.

---
+++ src/main/java/com/libertymutual/rpncalc/controllers/HomeController.java
@@ -0,0 +1,17 @@
+package com.libertymutual.rpncalc.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("")
+public class HomeController {
+	
+	@GetMapping("")
+	public String showCalculatorHomePage() {
+		return "redirect:/calculator";
+	}
+
+
+}
